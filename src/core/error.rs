8bb303762cecd86fff271556d1b2626ff8122451
Glauber Brennon <glauberbrennon@gmail.com>
2026-08-03 20:38:17 -0300

    test(core): add unit tests

--- src/core/error.rs
+++ src/core/error.rs
@@ -49,3 +49,100 @@ impl From<&str> for Error {
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_from_string() {
+        let err = Error::new("boom".to_string());
+        assert_eq!(err.to_string(), "boom");
+    }
+
+    #[test]
+    fn new_from_str() {
+        let err = Error::new("boom");
+        assert_eq!(err.to_string(), "boom");
+    }
+
+    #[test]
+    fn display_formats_message() {
+        let err = Error::new("something went wrong");
+        assert_eq!(format!("{err}"), "something went wrong");
+    }
+
+    #[test]
+    fn debug_includes_message() {
+        let err = Error::new("bad");
+        let debug = format!("{err:?}");
+        assert!(
+            debug.contains("bad"),
+            "debug output should contain the message: {debug}"
+        );
+    }
+
+    #[test]
+    fn std_error_source_returns_none() {
+        let err = Error::new("nope");
+        let boxed: Box<dyn std::error::Error> = Box::new(err);
+        assert!(boxed.source().is_none());
+    }
+
+    #[test]
+    fn from_io_error() {
+        let io = std::io::Error::new(std::io::ErrorKind::NotFound, "file not found");
+        let err = Error::from(io);
+        assert!(err.to_string().contains("file not found"));
+    }
+
+    #[test]
+    fn from_io_error_via_into() {
+        let io = std::io::Error::new(std::io::ErrorKind::PermissionDenied, "denied");
+        let err: Error = io.into();
+        assert!(err.to_string().contains("denied"));
+    }
+
+    #[test]
+    fn from_string() {
+        let err = Error::from("owned".to_string());
+        assert_eq!(err.to_string(), "owned");
+    }
+
+    #[test]
+    fn from_string_via_into() {
+        let err: Error = "owned".to_string().into();
+        assert_eq!(err.to_string(), "owned");
+    }
+
+    #[test]
+    fn from_str_ref() {
+        let err = Error::from("static str");
+        assert_eq!(err.to_string(), "static str");
+    }
+
+    #[test]
+    fn from_str_ref_via_into() {
+        let err: Error = "static str".into();
+        assert_eq!(err.to_string(), "static str");
+    }
+
+    #[test]
+    fn result_ok_variant_works() {
+        let res: Result<i32> = Ok(7);
+        assert_eq!(res.unwrap(), 7);
+    }
+
+    #[test]
+    fn result_err_variant_works() {
+        let res: Result<i32> = Err(Error::new("fail"));
+        assert!(res.is_err());
+        assert_eq!(res.unwrap_err().to_string(), "fail");
+    }
+
+    #[test]
+    fn error_is_send_sync() {
+        fn assert_send_sync<T: Send + Sync>() {}
+        assert_send_sync::<Error>();
+    }
+}
